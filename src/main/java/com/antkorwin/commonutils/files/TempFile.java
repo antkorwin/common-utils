3bb03b29d1b4e3e71f99f7d0c02a6ab7b735dd6f
Anatoliy Korovin <antkorwin@gmail.com>
2020-04-18 20:18:01 +1000

    simple refactoring of the TempFile internals

--- src/main/java/com/antkorwin/commonutils/files/TempFile.java
+++ src/main/java/com/antkorwin/commonutils/files/TempFile.java
@@ -2,6 +2,7 @@ package com.antkorwin.commonutils.files;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.file.Files;
@@ -36,44 +37,6 @@ public class TempFile {
      * For example, you can read this file multiple times in the body of processing function.
      * <p>
      * After the exit of the processing function, the temporary file will be deleted.
-     *
-     * @param processingFunction   the processing function for the file which built from the input stream
-     * @param <ConsumerReturnType> the type of processing function result
-     * @return the result of processing function
-     */
-    public <ConsumerReturnType> ConsumerReturnType evaluate(ThrowableFunction<File, ConsumerReturnType> processingFunction) {
-        File tempFile = null;
-        try {
-            tempFile = Files.createTempFile(UUID.randomUUID().toString(), "tmp").toFile();
-
-            // copy original data into the temp file
-            try (InputStream inputStream = inputStreamSupplier.get();
-                 OutputStream outputStream = new FileOutputStream(tempFile)) {
-                IOUtils.copy(inputStream, outputStream);
-            }
-
-            // run the processing function
-            return processingFunction.apply(tempFile);
-
-        } catch (RuntimeException exc) {
-            throw exc;
-        } catch (Throwable ex) {
-            throw new WrappedException(ex);
-        } finally {
-            if (tempFile != null) {
-                tempFile.delete();
-            }
-        }
-    }
-
-
-    /**
-     * This method saves data from the input stream into the temporary file
-     * then you can make something with this file independently of the input stream.
-     * <p>
-     * For example, you can read this file multiple times in the body of processing function.
-     * <p>
-     * After the exit of the processing function, the temporary file will be deleted.
      * <br><br>
      * Same as the {@link #evaluate(ThrowableFunction)} but without the necessary to return a value.
      *
@@ -84,5 +47,61 @@ public class TempFile {
             processing.accept(f);
             return null;
         });
+    }
+
+    /**
+     * This method saves data from the input stream into the temporary file
+     * then you can make something with this file independently of the input stream.
+     * <p>
+     * For example, you can read this file multiple times in the body of processing function.
+     * <p>
+     * After the exit of the processing function, the temporary file will be deleted.
+     *
+     * @param processingFunction   the processing function for the file which built from the input stream
+     * @param <ConsumerReturnType> the type of processing function result
+     * @return the result of processing function
+     */
+    public <ConsumerReturnType> ConsumerReturnType evaluate(ThrowableFunction<File, ConsumerReturnType> processingFunction) {
+        File tempFile = null;
+        try {
+            tempFile = createTempFile();
+            copyDataFromStreamToFile(tempFile);
+            return processingFunction.apply(tempFile);
+        } finally {
+            if (tempFile != null) {
+                tempFile.delete();
+            }
+        }
+    }
+
+    /**
+     * Create a temporary file with a random name
+     *
+     * @return return create file
+     */
+    private File createTempFile() {
+        try {
+            return Files.createTempFile(UUID.randomUUID().toString(), "tmp").toFile();
+        } catch (IOException e) {
+            throw new WrappedException(e);
+        }
+    }
+
+    /**
+     * Copy an original data form the InputStream into the temp file.
+     *
+     * @param tempFile in this file will be copy data from {@link #inputStreamSupplier}
+     * @throws IOException exception which may happened while you read a data from stream
+     */
+    private void copyDataFromStreamToFile(File tempFile) {
+        try {
+            try (InputStream inputStream = inputStreamSupplier.get();
+                 OutputStream outputStream = new FileOutputStream(tempFile)) {
+
+                IOUtils.copy(inputStream, outputStream);
+            }
+        } catch (IOException e) {
+            throw new WrappedException(e);
+        }
     }
 }
